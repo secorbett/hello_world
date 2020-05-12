7e5fc9041bab1813216f815c0338b6b5598d8def
secorbett <secorbett@me.com>
2020-05-11 22:46:46 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,11 +5,11 @@ public class hello {
     public static void main(String [] args) {
 
         // initialize variables
-        String firstName = "Elizabeth";
-        String lastName = "Jones";
+        String firstName = "Sophie ";
+        String lastName = "Corbett ";
 	
 		// output welcome message
-        System.out.println("Hello Class!");
+        System.out.println("Hello, my name is "+ firstName + lastName);
 
 
     }
